ac0b699c636be74a318307999dc04dc7dc1eee8c
nknorr <nicolas.knorr@gmail.com>
2016-05-24 22:15:59 +0200

    Ajout de la fonction built in cd
    Ajout du squelette des commandes built in tel que cd et ajout de la commande cd

--- mybash.c
+++ mybash.c
@@ -17,7 +17,47 @@ void handle_signal(int signal) {
 		exit(0);
 	}
 }
+//Commande cd
+int lsh_cd(char **args)
+{
+	if(args[1] == NULL) {
+		fprintf(stderr, "> Argument attendu pour cd \n");
+	} else {
+		if(chdir(args[1]) != 0) {
+			perror("lsh");
+		}
+	}
+	return 1;
+}
+//fonction permetant de "reconnaitre" la commande cd
+char *builtin_str[] = {
+	"cd"
+};
 
+int (*builtin_func[]) (char **) = {
+	&lsh_cd
+};
+
+int lsh_num_builtins() {
+	return sizeof(builtin_str) / sizeof(char *);
+}
+
+
+//Fonction qui permet d'éxécuter une commande builtin (cf:cd) ou sinon une commande forké (ls etc...)
+int lsh_exec(char *params)
+{
+	 int i,j;
+	char **paramz; 
+	List chain = createChainFromString(params);
+	j=printCommand(chain->c);//cette ligne là n'est que pour le test
+	paramz = getParamsAsArray(chain);
+	for(i = 0; i< lsh_num_builtins(); i++){
+			if(strcmp(paramz[0], builtin_str[i]) == 0) {
+				return (*builtin_func[i])(paramz);
+			}
+		}
+		return executeChain(chain);
+}
 int main() {
 
 	struct sigaction sa;
@@ -29,13 +69,15 @@ int main() {
 	printf("Bienvenue à \"mybash\". \nEntrez les commandes comme sur votre bash habituel.\n");
 	char *myCommand = malloc(MAX_SIZE*sizeof(char));
 	while(1) {
+		int i = 0;
 		printf("> ");
 		fflush(stdout);
-
-		scanf("%[^\n]%*c", myCommand);
 		
-		List chain = createChainFromString(myCommand);
-		executeChain(chain);
+		scanf("%[^\n]%*c", myCommand);
+		i = lsh_exec(myCommand);
+
+		/*List chain = createChainFromString(myCommand);
+		executeChain(chain);*/
 
 // VERSION 1.0, SEULEMENT 1 COMMANDE
 		/*pid_t pid = fork();
